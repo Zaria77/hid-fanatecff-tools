99949f7f47c43a82c93e9e0046e4e7f4504e19a0
Zaria <z@ria.sh>
2025-04-13 15:45:48 -0400

    Add super basic code to disable RevStripe on certain wheels in new utilities.py script.

---
+++ tools/utilities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys
+
+sys.path.append("../dbus")
+import fanatec_input
+
+def turn_off_light() :
+    "The fanatec_input module provides the CSLP1V2Wheel class for controlling the RevStripe™© on equipped models. This function is a quick hack to turn the stripe off without modifying the driver itself."
+
+    wheel = fanatec_input.CSLP1V2Wheel()
+
+    wheel.set_sysfs_rpm(99)
+    wheel.set_sysfs_rpm(0)
+
+if __name__ == "__main__" :
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Random utilities for Fanatec wheels"
+    )
+    parser.add_argument(
+        '-l', '--turn_off_light',
+        action='store_true',
+        help="Attempt to disable RevStripe for, e.g., CSL WRC wheel so it's not shining in your face.")
+    args = parser.parse_args()
+
+    if args.turn_off_light :
+        turn_off_light()
